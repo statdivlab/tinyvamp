c1e65dd7fb65f77fe7e9a006648774c83e65133e
amy <amy.willis7@gmail.com>
2022-06-14 17:21:13 -0700

    first draft of vignette

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' The composition of 10 spike-in taxa in 19 samples observed from flow cytometry. 
+#' 
+#' For details, see Costea et al (2017). 
+#'
+#'
+#' @source \url{https://doi.org/10.1038/nbt.3960}
+"costea2017_mock_composition"
